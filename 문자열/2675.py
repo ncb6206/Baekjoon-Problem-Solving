c2f150df12ff1054c4d60e946286f4a5c269f37d
ncb6206 <ncb6206@gmail.com>
2022-12-18 16:35:33 +0900

    파이썬 백준 2675번 풀이

--- 문자열/2675.py
+++ 문자열/2675.py
@@ -1,3 +1,4 @@
+# 직접 푼 알고리즘
 T = int(input())
 
 for _ in range(T):
@@ -7,3 +8,15 @@ for _ in range(T):
         for _ in range(int(R)):
             print(i,end="")
     print("")
+
+# 인터넷에서 찾은 정답 알고리즘
+'''
+T = int(input())
+
+for i in range(T):
+    num, s = input().split()
+    text = ''
+    for i in s:
+        text += int(num) * i
+    print(text)
+'''
