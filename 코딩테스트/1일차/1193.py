e1706595b77f898f7533a3c393ad1d85fa868ce0
ncb6206 <ncb6206@gmail.com>
2024-03-27 18:25:55 +0900

    1193번 코드 수정

--- 코딩테스트/1일차/1193.py
+++ 코딩테스트/1일차/1193.py
@@ -1,43 +1,14 @@
 import sys
 
+line = 1
 X = int(sys.stdin.readline())
 
-bunza = 1
-bunmo = 1
-bunzaMinusbunmoPlusCount = 0
-bunzaPlusbunmoMinusCount = 0
+while X > line :
+    X -= line
+    line += 1
 
-def bunzaMinusbunmoPlus(x1,x2):
-    global bunzaMinusbunmoPlusCount, bunzaPlusbunmoMinusCount
-    x2 += 1
-    if x1 - 1 > 0:
-        x1 -= 1
-    else:
-        bunzaMinusbunmoPlusCount = 0
-        bunzaPlusbunmoMinusCount = 1
+if line % 2 == 0:
+    print(f"{X}/{line-X+1}")
+else:
+    print(f"{line-X+1}/{X}")
 
-    return x1,x2
-
-def bunzaPlusbunmoMinus(x1,x2):
-    global bunzaMinusbunmoPlusCount, bunzaPlusbunmoMinusCount
-    x1 += 1
-    if x2 - 1 > 0:
-        x2 -= 1
-    else:
-        bunzaMinusbunmoPlusCount = 1
-        bunzaPlusbunmoMinusCount = 0
-
-    return x1,x2
-
-
-for i in range(1,X):
-    if i == 1:
-        bunza, bunmo = bunzaMinusbunmoPlus(bunza, bunmo)
-
-    elif bunzaMinusbunmoPlusCount == 1:
-        bunza, bunmo = bunzaMinusbunmoPlus(bunza, bunmo)
-
-    elif bunzaPlusbunmoMinusCount == 1:
-        bunza, bunmo = bunzaPlusbunmoMinus(bunza, bunmo)
-
-print(f"{bunza}/{bunmo}")
