f886e92574ca460fb6800a506c2e524fab6b1df8
ncb6206 <ncb6206@gmail.com>
2024-03-30 00:38:39 +0900

    백트래킹 로직을 사용한 거 (내가 푼 건 아님)

--- 코딩테스트/3일차/15650.py
+++ 코딩테스트/3일차/15650.py
@@ -4,9 +4,24 @@ input = sys.stdin.readline
 
 N,M = map(int,input().split())
 array = list()
+answer = []
+answers = []
 
-for i in range(N):
-    array.append(i+1)
+# for i in range(N):
+#     array.append(i+1)
+#
+# for j in combinations(array,M):
+#     print(*j)
 
-for j in combinations(array,M):
-    print(*j)
+def back_tracking(index,n,m,start):
+    if index == m:
+        answers.append(' '.join(map(str,answer)))
+        return
+    for i in range(start,n):
+        answer.append(i+1)
+        back_tracking(index+1,n,m,i+1)
+        answer.pop()
+
+back_tracking(0,N,M,0)
+
+print('\n'.join(answers))
--- 코딩테스트/3일차/15663.py
+++ 코딩테스트/3일차/15663.py
@@ -1,25 +1,23 @@
 import sys
-from itertools import permutations
 input = sys.stdin.readline
 
-N, M = map(int,input().split())
+N,M = map(int,input().split())
+array = sorted(list(map(int,input().split())))
 visited = [False] * N
-answers = []
 answer = []
-# for i in range(N):
-#     array.append(i+1)
-#
-# for dap in permutations(array,M):
-#     print(*dap)
+answers = []
 
 def back_tracking(index,n,m):
     if index == m:
         answers.append(' '.join(map(str,answer)))
         return
+    # remember는 각 자리 수를 기준으로 초기화 되서 9 9 같은 것도 된다
+    remember = 0
     for i in range(n):
-        if not visited[i]:
+        if not visited[i] and remember != array[i]:
             visited[i] = True
-            answer.append(i+1)
+            answer.append(array[i])
+            remember = array[i]
             back_tracking(index+1,n,m)
             visited[i] = False
             answer.pop()
