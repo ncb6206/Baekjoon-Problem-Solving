23973891dfa5ebad64b6e2feb4f05851ab15b81c
ncb6206 <ncb6206@gmail.com>
2024-04-06 21:31:59 +0900

    코딩테스트 9일차

--- 코딩테스트/9일차/19638.py
+++ 코딩테스트/9일차/19638.py
@@ -1,2 +1,29 @@
 import sys
+from heapq import *
 input = sys.stdin.readline
+
+# 거인의 수 n, 센티의 키 centi, 마법의 뿅망치 횟수 제한 t를 입력 받음
+n, centi, t = map(int,input().split())
+giant = [-int(input()) for _ in range(n)]  # 거인들의 키를 최소 힙에 추가하기 위해 음수로 변환하여 리스트에 삽입
+heapify(giant)  # 거인들의 키 리스트를 힙큐로 변경
+
+count = 0   # 뿅망치가 사용된 횟수를 저장할 변수 초기화
+maxHeight = -heappop(giant)     # 힙에서 가장 큰 값(절대값이 가장 작은 값 = 가장 큰 거인의 키)을 꺼냄
+
+# t번의 시뮬레이션을 수행
+for _ in range(t):
+    # 만약 가장 큰 거인의 키가 1이거나 최소 높이 이하라면 반복문을 종료
+    if maxHeight == 1 or maxHeight < centi:
+        break
+    else:
+        # 현재 가장 큰 거인의 키를 절반으로 줄이고 음수로 변환해서 힙에 추가
+        heappush(giant, -(maxHeight // 2))
+        count += 1                  # 뿅망치가 사용된 횟수 1회 추가
+    maxHeight = -heappop(giant)     # 다음 가장 큰 키를 가져옴
+
+# 만약 가장 큰 거인의 키가 센티의 키보다 크거나 같다면 'NO'와 가장 큰 거인의 키를 출력
+if maxHeight >= centi:
+    print('NO',maxHeight,sep='\n')
+# 그렇지 않다면 'YES'와 최소로 뿅망치가 사용된 횟수를 출력
+else:
+    print('YES',count,sep='\n')
--- 코딩테스트/9일차/2304.py
+++ 코딩테스트/9일차/2304.py
@@ -1,2 +1,41 @@
+# 전체 넓이를 구해서 지붕이 설치 안된 나머지 부분의 넓이를 뺀 값을 구하려고 했지만 실패
+
 import sys
 input = sys.stdin.readline
+
+# 막대 기둥 개수 n을 입력받음
+n = int(input())
+
+# 각 직사각형의 왼쪽 아래 모서리 좌표와 높이를 입력 받고 왼쪽 아래 모서리 좌표를 기준으로 정렬
+square = [tuple(map(int,input().split())) for _ in range(n)]
+square.sort()
+
+# 가장 높은 직사각형의 높이와 인덱스를 초기화
+maxHeight = 0
+maxIdx = 0
+area = 0
+
+# 가장 높은 직사각형의 높이와 인덱스를 찾음
+for i in range(n):
+    if maxHeight <= square[i][1]:
+        maxHeight = square[i][1]
+        maxIdx = i
+
+
+# 왼쪽에서부터 가장 높은 직사각형까지의 넓이를 계산
+currentHeight = 0
+for j in range(0,maxIdx):
+    currentHeight = max(currentHeight,square[j][1])
+    area += (square[j+1][0] - square[j][0]) * currentHeight
+    
+# 오른쪽에서부터 가장 높은 직사각형까지의 넓이를 계산
+currentHeight = 0
+for j in range(n-1,maxIdx,-1):
+    currentHeight = max(currentHeight,square[j][1])
+    area += (square[j][0] - square[j-1][0]) * currentHeight
+    
+# 가장 높은 직사각형의 넓이를 추가
+area += maxHeight
+
+# 결과를 출력
+print(area)
