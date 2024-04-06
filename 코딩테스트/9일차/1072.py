e32db192d287f05b785decdeb88c8539b016354d
ncb6206 <ncb6206@gmail.com>
2024-04-06 17:51:41 +0900

    코딩테스트 9일차

--- 코딩테스트/9일차/1072.py
+++ 코딩테스트/9일차/1072.py
@@ -1,13 +1,31 @@
 import sys
 input = sys.stdin.readline
 
+# 초기 게임 횟수와 이긴 게임 수 입력 받음
 x,y = map(int,input().split())
 
-left = 1
-right = 10 ** 9 + x
-winning = y//x * 100
+# 초기 승률 계산
+winning = y * 100 // x 
 
-# while left <= right:
-#     mid = ( left + right ) // 2
+# 이진 탐색을 위한 초기 left와 right 값 설정
+left = 1
+right = 10 ** 9
+
+# 승률이 변하지 않으면 -1을 출력 하기 위해 -1로 초기화
+result = -1
+
+# 이진 탐색 수행
+while left <= right:
+    mid = ( left + right ) // 2                 # 중간 값 계산
+    diff_winning = (y+mid) * 100 // (x+mid)     # 현재 승률 계산
     
-print(winning)
+    # 현재 승률이 초기 승률과 다른 경우,결과를 갱신하고 더 작은 중간 값으로 이동하기 위해 right값 변경
+    if diff_winning != winning:                 
+        result = mid
+        right = mid - 1
+    # 현재 승률이 초기 승률과 같은 경우, 더 큰 중간 값으로 이동하기 위해 left값 변경
+    else:
+        left = mid + 1
+    
+# 최소 몇 판 더해야 하는 지 결과 출력
+print(result)
--- 코딩테스트/9일차/2108.py
+++ 코딩테스트/9일차/2108.py
@@ -1,2 +1,33 @@
 import sys
+from collections import Counter
 input = sys.stdin.readline
+
+# 숫자의 개수 n을 입력받음
+n = int(input())
+
+# 숫자 리스트를 입력받고 정렬
+numList = [int(input()) for _ in range(n)]
+numList.sort()
+
+# 산술평균을 구함
+average = round(sum(numList) / n)
+
+# 중앙값을 구함
+median = numList[n//2]
+
+# 숫자 리스트에서 각 숫자의 개수를 세고, 가장 많이 나오는 두 숫자를 찾아 배열로 반환 (ex : [(1, 3), (2, 2)] )
+counterList = Counter(numList).most_common(2)
+
+# 최빈값을 구함
+if len(counterList) == 1:                       # 숫자 리스트 배열의 개수가 1개면 그 숫자 반환
+    frequent = counterList[0][0]
+elif counterList[0][1] == counterList[1][1]:    # 개수가 가장 많이 나오는 두 숫자의 개수가 같으면 두번째로 작은 수 반환
+    frequent = counterList[1][0]
+else:                                           # 개수가 가장 많이 나온 수 반환
+    frequent = counterList[0][0]
+    
+# 범위를 구함
+scope = numList[-1] - numList[0]
+
+# 결과를 출력
+print(average, median, frequent, scope, sep='\n')
