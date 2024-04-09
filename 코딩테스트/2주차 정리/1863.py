803d1602b6100f409f0041108702cbe1f01dc44e
ncb6206 <ncb6206@gmail.com>
2024-04-09 13:08:31 +0900

    코딩테스트 2주차

--- 코딩테스트/2주차 정리/1863.py
+++ 코딩테스트/2주차 정리/1863.py
@@ -3,8 +3,29 @@ input = sys.stdin.readline
 
 n = int(input())
 skyline = []
+stack = []
+count = 0
 
 for _ in range(n):
-    skyline.append(list(map(int,input().split())))
+    x,y = map(int,input().split())
+    if not stack:
+        stack.append(y)
+        count += 1
+    elif y > 0:
+        if y > skyline[-1][1]:
+            stack.append(y)
+            count += 1
+        else:
+            if y in stack:
+                if stack[-1] != y:
+                    stack.pop()
+            else:
+                stack.pop()
+                stack.append(y)
+                count += 1
+    else:
+        stack.clear()
+    skyline.append([x,y])
+    # print(stack,count)
     
-print(skyline)
+print(count)
