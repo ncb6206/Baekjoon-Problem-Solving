05fc19b8ef54048305ce76f857e487a5e723d02e
ncb6206 <ncb6206@gmail.com>
2024-04-16 11:59:17 +0900

    코딩테스트 3주차

--- 코딩테스트/3주차 정리/10282.py
+++ 코딩테스트/3주차 정리/10282.py
@@ -1,2 +1,46 @@
 import sys
+from heapq import *
 input = sys.stdin.readline
+
+T = int(input())
+result = []
+INF = float('inf')
+
+def dikstra(start):
+    dist = [INF] * (n+1)
+    dist[start] = 0
+    queue = [(0,start)]
+    
+    while queue:
+        cost,idx = heappop(queue)
+        if dist[idx] < cost:
+            continue
+        
+        for adj, d in graph[idx]:
+            if dist[adj] > dist[idx] + d: 
+                dist[adj] = dist[idx] + d
+                heappush(queue,(dist[adj],adj))
+                
+    return dist
+    
+for _ in range(T):
+    n,d,c = map(int,input().split())
+    graph = [[] for _ in range(n+1)]
+
+    for _ in range(d):
+        a,b,s = map(int,input().split())
+        graph[b].append([a,s])
+    count = 0
+    time = 0
+    dist = dikstra(c)
+    
+    for i in dist:
+        if i != INF:
+            count += 1
+            time = max(time,i)
+            
+    # print(dist)
+    result.append([count,time])
+    
+for i in range(T):
+    print(*result[i])
--- 코딩테스트/3주차 정리/2212.py
+++ 코딩테스트/3주차 정리/2212.py
@@ -1,2 +1,21 @@
 import sys
 input = sys.stdin.readline
+
+N = int(input())
+K = int(input())
+sensor = list(map(int,input().split()))
+
+if N <= K:
+    print(0)
+    exit(0)
+
+sensor.sort()
+diff = []
+
+for i in range(1,N):
+    diff.append(sensor[i]-sensor[i-1])
+    
+diff.sort()
+result = diff[:(N-K)]
+
+print(sum(result))
